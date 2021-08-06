11cf702f9a524f8bb5dd9774b2f36dafd7dfef5e
PauloDev13 <devpgm2020@gmail.com>
2021-08-06 14:43:43 -0300

    Configurando o projeto

---
+++ src/pages/index.tsx
@@ -0,0 +1,13 @@
+import React from 'react';
+
+export default function Home() {
+  return (
+    <div
+      className={`
+        bg-gradient-to-r from-green-500 to-blue-500
+    `}
+    >
+      <h1>Index</h1>
+    </div>
+  );
+}
