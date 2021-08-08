89dcbd6c84c1af57a66f4bf66c12282b3321b6cf
PauloDev13 <devpgm2020@gmail.com>
2021-08-07 21:24:11 -0300

    Ajustando o componente Título

--- src/components/template/MenuLateral.tsx
+++ src/components/template/MenuLateral.tsx
@@ -1,9 +1,15 @@
 import MenuItem from './MenuItem';
 import { IconeAjustes, IconeCasa, IconeSino } from '../icons';
+import Logo from './Logo';
 
 const MenuLateral = () => {
   return (
     <aside>
+      <div
+        className={`flex flex-col items-center justify-center bg-gradient-to-r from-indigo-500 to-purple-800 h-20 w-20`}
+      >
+        <Logo />
+      </div>
       <MenuItem url={'/'} texto={'Inicio'} icone={IconeCasa} />
       <MenuItem url={'/ajustes'} texto={'Ajustes'} icone={IconeAjustes} />
       <MenuItem
