ffbe5fe916b6fbcf21c5de21b9a173858757d9f3
PauloDev13 <devpgm2020@gmail.com>
2021-08-09 19:22:56 -0300

    Implantando autenticação com Google

--- src/data/context/AuthContext.tsx
+++ src/data/context/AuthContext.tsx
@@ -1,8 +1,9 @@
-import { createContext, useState } from 'react';
+import {createContext, useState} from 'react';
 import route from 'next/router';
-import { NextPage } from 'next';
+import {NextPage} from 'next';
 
 import Usuario from '../../model/Usuario';
+import firebase from '../../firebase/config';
 
 interface AuthContextProps {
   usuario?: Usuario;
@@ -11,28 +12,38 @@ interface AuthContextProps {
 
 const AuthContext = createContext<AuthContextProps>({});
 
-// const UsuarioNormalizado = async (
-//   usuarioFirebase: firebase.User,
-// ): Promise<Usuario> => {
-//   const token = await usuarioFirebase.getIdToken();
-//   const { uid, displayName, email, providerData, photoURL } = usuarioFirebase;
-//
-//   return {
-//     uid,
-//     nome: displayName,
-//     email,
-//     token,
-//     provedor: providerData[0]?.providerId,
-//     imagemUrl: photoURL,
-//   };
-// };
+const UsuarioNormalizado = async (
+  usuarioFirebase: firebase.User,
+): Promise<Usuario> => {
+  const token = await usuarioFirebase.getIdToken();
+  const {
+    uid, displayName, email,
+    providerData, photoURL
+  } = usuarioFirebase;
+
+  return {
+    uid,
+    nome: displayName,
+    email,
+    token,
+    provedor: providerData[0]?.providerId,
+    imagemUrl: photoURL,
+  };
+};
 
 export const AuthProvider: NextPage = ({ children }) => {
   const [usuario, setUsuario] = useState<Usuario>();
 
   const loginGoogle = async () => {
-    console.log('login google...');
-    await route.push('/');
+    const resp = await firebase
+    .auth()
+    .signInWithPopup(new firebase.auth.GoogleAuthProvider());
+
+    if (resp.user?.email) {
+      const usuario = await UsuarioNormalizado(resp.user);
+      setUsuario(usuario);
+      await route.push('/');
+    }
   };
 
   return (
