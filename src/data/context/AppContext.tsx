93a1542f8e3f3f7c0d29c8b463691f3e10d1df3d
PauloDev13 <devpgm2020@gmail.com>
2021-08-11 17:38:57 -0300

    Salvando tema no localStorage

--- src/data/context/AppContext.tsx
+++ src/data/context/AppContext.tsx
@@ -1,21 +1,28 @@
-import { createContext, useState } from 'react';
-import { NextPage } from 'next';
+import {createContext, useEffect, useState} from 'react';
+import {NextPage} from 'next';
 
-type Tema = 'dark' | '';
+// type Tema = 'dark' | '';
 
 interface AppContextProps {
-  tema?: Tema;
+  tema?: string;
   alternarTema?: () => void;
 }
 
 const AppContext = createContext<AppContextProps>({});
 
 export const AppProvider: NextPage = ({ children }) => {
-  const [tema, setTema] = useState<Tema>('dark');
+  const [tema, setTema] = useState('dark');
 
   function alternarTema() {
-    setTema(tema === '' ? 'dark' : '');
+    const novoTema = tema === '' ? 'dark' : '';
+    setTema(novoTema);
+    localStorage.setItem('tema', novoTema);
   }
+
+  useEffect(() => {
+    const temaSalvo = localStorage.getItem('tema');
+    setTema(temaSalvo!);
+  }, []);
 
   return (
     <AppContext.Provider
